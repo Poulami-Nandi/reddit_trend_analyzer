85639133863e8f561e507530b8ce383f81c7eb9f
Poulami-Nandi <164754016+Poulami-Nandi@users.noreply.github.com>
2025-05-14 15:47:12 +0200

    Update __init__.py

--- reddit_trend_analyzer/__init__.py
+++ reddit_trend_analyzer/__init__.py
@@ -1,5 +1,9 @@
 """
-Reddit Trend Analyzer package.
+Reddit Hashtag/Keyword Trend Analyzer package.
 """
 
 __version__ = '0.1.0'
+
+from .collect_data import main as collect_data_main
+from .analyze_data import main as analyze_data_main
+from .query_data import main as query_data_main
